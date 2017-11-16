54ff86325711632dc36a48fdc47ecb487d8226a2
hstfls <33678841+hstfls@users.noreply.github.com>
2017-11-16 10:15:48 -0300

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -6,8 +6,8 @@ jQuery(document).ready(function () {
     //this code is for the gmap
     var map = new GMaps({
         el: '#map',
-        lat: -12.043333,
-        lng: -77.028333
+        lat: 40.7143528,
+        lng: -74.0059731
     });
 
 
